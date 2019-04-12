62a479e967d0f1bd46149adeda7dc4a34b22c65c
Alberta <albertawilliams@Albertas-MacBook-Pro.local>
2019-04-12 13:20:04 -0500

    story1.2 view a list of all books

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -2,4 +2,5 @@ package com.twu.biblioteca;
 
 public class Library {
     public String message = "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore!";
+    public String books[] = {"Book 1", "Book 2", "Book 3"};
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ExampleTest {
 
@@ -14,6 +15,19 @@ public class ExampleTest {
     @Test
     public void testMessage() {
         String message = new Library().message;
+
         assertEquals(message, "Welcome to Biblioteca. Your one-stop shop for great book titles in Bangalore!");
+    }
+
+    @Test
+    public void testView() {
+        String libraryBooks[] = new Library().books;
+
+        String books[] = new String[3];
+        books[0] = "Book 1";
+        books[1] = "Book 2";
+        books[2] = "Book 3";
+
+        assertArrayEquals(books, libraryBooks);
     }
 }
